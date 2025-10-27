cffc38cdfe240fdd4c36bacf667bccc51ab29696
Eleazar Pequeno <edpu0033@gmail.com>
2025-10-26 21:13:52 -0500

    Add Scene Senses command

--- lua/tangled-worlds/tables/scene.lua
+++ lua/tangled-worlds/tables/scene.lua
@@ -2,5 +2,6 @@ Scene = {}
 
 Scene.Challenge = require('tangled-worlds.tables.scene-challenge')
 Scene.Reaction = require('tangled-worlds.tables.scene-reaction')
+Scene.Senses = require('tangled-worlds.tables.scene-senses')
 
 return Scene
