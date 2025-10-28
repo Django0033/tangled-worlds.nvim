725dbf75fad20c5944cdb4dd27c5c954ef5f6303
Eleazar Pequeno <edpu0033@gmail.com>
2025-10-27 21:47:22 -0500

    Add Scene Activity command

--- lua/tangled-worlds/tables/scene.lua
+++ lua/tangled-worlds/tables/scene.lua
@@ -3,5 +3,6 @@ Scene = {}
 Scene.Challenge = require('tangled-worlds.tables.scene-challenge')
 Scene.Reaction = require('tangled-worlds.tables.scene-reaction')
 Scene.Senses = require('tangled-worlds.tables.scene-senses')
+Scene.Activity = require('tangled-worlds.tables.scene-activity')
 
 return Scene
