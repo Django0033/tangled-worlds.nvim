400b42d7955f098a05eabed018f98f9ffea45ec9
Eleazar Pequeno <edpu0033@gmail.com>
2025-10-29 22:37:29 -0500

    Add the Scene Detail command

--- lua/tangled-worlds/tables/scene.lua
+++ lua/tangled-worlds/tables/scene.lua
@@ -4,5 +4,6 @@ Scene.Challenge = require('tangled-worlds.tables.scene-challenge')
 Scene.Reaction = require('tangled-worlds.tables.scene-reaction')
 Scene.Senses = require('tangled-worlds.tables.scene-senses')
 Scene.Activity = require('tangled-worlds.tables.scene-activity')
+Scene.Detail = require('tangled-worlds.tables.scene-detail')
 
 return Scene
