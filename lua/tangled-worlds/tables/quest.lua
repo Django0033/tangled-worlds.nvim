e74756cfe49aeebba2c0ecee586d6d21110f5488
Eleazar Pequeno <edpu0033@gmail.com>
2025-11-08 11:01:13 -0500

    Add Quest Escalation and Reward commands

--- lua/tangled-worlds/tables/quest.lua
+++ lua/tangled-worlds/tables/quest.lua
@@ -4,5 +4,7 @@ Quest.Mission = require("tangled-worlds.tables.quest-mission")
 Quest.Oposition = require("tangled-worlds.tables.quest-oposition")
 Quest.Hindrance = require("tangled-worlds.tables.quest-hindrance")
 Quest.Aid = require("tangled-worlds.tables.quest-aid")
+Quest.Escalation = require("tangled-worlds.tables.quest-escalation")
+Quest.Reward = require("tangled-worlds.tables.quest-reward")
 
 return Quest
