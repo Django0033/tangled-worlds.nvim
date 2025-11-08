44b7128044041999bf3da0618cb079c594e81133
Eleazar Pequeno <edpu0033@gmail.com>
2025-11-08 09:44:08 -0500

    Add Quest Hindrance and Aid command

--- lua/tangled-worlds/tables/quest.lua
+++ lua/tangled-worlds/tables/quest.lua
@@ -1,6 +1,8 @@
 Quest = {}
 
-Quest.Mission = require('tangled-worlds.tables.quest-mission')
-Quest.Oposition = require('tangled-worlds.tables.quest-oposition')
+Quest.Mission = require("tangled-worlds.tables.quest-mission")
+Quest.Oposition = require("tangled-worlds.tables.quest-oposition")
+Quest.Hindrance = require("tangled-worlds.tables.quest-hindrance")
+Quest.Aid = require("tangled-worlds.tables.quest-aid")
 
 return Quest
