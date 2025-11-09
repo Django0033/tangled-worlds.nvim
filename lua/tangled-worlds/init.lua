d980cce349bd6ff11d35db1618a3c35ea9231405
Eleazar Pequeno <edpu0033@gmail.com>
2025-11-08 23:50:39 -0500

    Refactor

--- lua/tangled-worlds/init.lua
+++ lua/tangled-worlds/init.lua
@@ -2,12 +2,12 @@ M = {}
 
 Tangled_tbls = {}
 
-Tangled_tbls.Creation = require('tangled-worlds.tables.creation')
-Tangled_tbls.Exploration = require('tangled-worlds.tables.exploration')
-Tangled_tbls.World = require('tangled-worlds.tables.world')
-Tangled_tbls.People = require('tangled-worlds.tables.people')
-Tangled_tbls.Scene = require('tangled-worlds.tables.scene')
-Tangled_tbls.Quest = require('tangled-worlds.tables.quest')
+Tangled_tbls.Creation = require("tangled-worlds.tables.creation")
+Tangled_tbls.Exploration = require("tangled-worlds.tables.exploration")
+Tangled_tbls.World = require("tangled-worlds.tables.world")
+Tangled_tbls.People = require("tangled-worlds.tables.people")
+Tangled_tbls.Scene = require("tangled-worlds.tables.scene")
+Tangled_tbls.Quest = require("tangled-worlds.tables.quest")
 
 local function get_random_number(top_number)
     local random_number = math.random(1, top_number)
@@ -28,7 +28,6 @@ local function get_random_element(tbl)
 end
 
 function M.print_random_elements(opts)
-
     local primary_tbl = Tangled_tbls[opts.fargs[1]]
     local secondary_tbl = primary_tbl[opts.fargs[2]]
 
@@ -36,24 +35,26 @@ function M.print_random_elements(opts)
         local tertiary_tbl1 = secondary_tbl[1]
         local tertiary_tbl2 = secondary_tbl[2]
 
-        print(get_random_element(tertiary_tbl1) .. '-' .. get_random_element(tertiary_tbl2))
-
+        print(get_random_element(tertiary_tbl1) .. "-" .. get_random_element(tertiary_tbl2))
     elseif #secondary_tbl == 3 then
         local tertiary_tbl1 = secondary_tbl[1]
         local tertiary_tbl2 = secondary_tbl[2]
         local tertiary_tbl3 = secondary_tbl[3]
 
-        print(get_random_element(tertiary_tbl1) .. '-' .. get_random_element(tertiary_tbl2) .. '-' .. get_random_element(tertiary_tbl3))
-
+        print(
+            get_random_element(tertiary_tbl1)
+            .. "-"
+            .. get_random_element(tertiary_tbl2)
+            .. "-"
+            .. get_random_element(tertiary_tbl3)
+        )
     else
         print(get_random_element(secondary_tbl))
-
     end
-
 end
 
-function M.dinamic_completer(arg_lead, cmd_line, cursor_pos)
-    local args = vim.fn.split(cmd_line, ' ')
+function M.dinamic_completer(_, cmd_line, _)
+    local args = vim.fn.split(cmd_line, " ")
 
     if #args < 2 then
         local keys = {}
@@ -67,7 +68,7 @@ function M.dinamic_completer(arg_lead, cmd_line, cursor_pos)
         local main_table_name = args[2]
         local sub_table = Tangled_tbls[main_table_name]
 
-        if sub_table and type(sub_table) == 'table' then
+        if sub_table and type(sub_table) == "table" then
             local keys = {}
 
             for k, _ in pairs(sub_table) do
