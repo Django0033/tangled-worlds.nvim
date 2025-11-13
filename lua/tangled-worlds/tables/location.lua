2729f3be0a256882f3d37605fb0462d28e9f8ef0
Eleazar Pequeno <edpu0033@gmail.com>
2025-11-12 21:39:54 -0500

    Add Location Trouble command

--- lua/tangled-worlds/tables/location.lua
+++ lua/tangled-worlds/tables/location.lua
@@ -2,5 +2,6 @@ Location = {}
 
 Location.PastPresent = require("tangled-worlds.tables.location-past-present")
 Location.Descriptor = require("tangled-worlds.tables.location-descriptor")
+Location.Trouble = require("tangled-worlds.tables.location-trouble")
 
 return Location
