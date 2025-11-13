2bb77b742aaa68a6d60537fc370e8c2143b6b25a
Eleazar Pequeno <edpu0033@gmail.com>
2025-11-12 22:21:34 -0500

    Add Location Building command

--- lua/tangled-worlds/tables/location.lua
+++ lua/tangled-worlds/tables/location.lua
@@ -3,5 +3,6 @@ Location = {}
 Location.PastPresent = require("tangled-worlds.tables.location-past-present")
 Location.Descriptor = require("tangled-worlds.tables.location-descriptor")
 Location.Trouble = require("tangled-worlds.tables.location-trouble")
+Location.Building = require("tangled-worlds.tables.location-building")
 
 return Location
