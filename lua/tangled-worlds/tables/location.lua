5b5e50206adac90a37668e3a219574a91ac788c0
Eleazar Pequeno <edpu0033@gmail.com>
2025-11-27 20:52:21 -0500

    Add Location Influence command

--- lua/tangled-worlds/tables/location.lua
+++ lua/tangled-worlds/tables/location.lua
@@ -4,5 +4,6 @@ Location.PastPresent = require("tangled-worlds.tables.location-past-present")
 Location.Descriptor = require("tangled-worlds.tables.location-descriptor")
 Location.Trouble = require("tangled-worlds.tables.location-trouble")
 Location.Building = require("tangled-worlds.tables.location-building")
+Location.Influence = require("tangled-worlds.tables.location-influence")
 
 return Location
